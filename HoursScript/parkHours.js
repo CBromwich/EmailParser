95cd348218abde156c5704e3b0090bec4142d86f
hotfries <cjbromwich@gmail.com>
2016-06-26 13:35:14 +0000

    Changed data output method from print to array

--- HoursScript/parkHours.js
+++ HoursScript/parkHours.js
@@ -3,12 +3,9 @@
 //Github url: https://github.com/CBromwich/parkHours 
 
 /*
+
 Dynamically retrieves park hours and EMH from the Disney World website
 
-TODO: 
- - Information currently only prints in console. Rework to return values instead of print them
- - Wrap in sharepoint API
-      
 */
 
 
@@ -18,6 +15,7 @@ var cheerio = require("cheerio");
 
 
 var url = "https://disneyworld.disney.go.com/calendars/"; //Disney"s official page for park hours
+var dataList = []; //Multidimensional array to hold arrays of each park field (Park name, hours EMH)
     
 request(url, function (error, response, html) {
     "use strict"; //so JSLint will stop yelling at me
@@ -27,16 +25,28 @@ request(url, function (error, response, html) {
         var $ = cheerio.load(html);
         
         //filter() creates a subset of the DOM using only the matching selectors (.clickRow in this case)
-        $(".clickRow").filter(function() { 
+        $(".clickRow").filter(function() {
+            var list = []; // holds the three data values (name, hours, emh). This list is appended to dataList
+            
             var data = $(this).children(); // data is the children of the .clickRow object in the DOM
             
             var name = data.first().text(); // get first child of .clickRow, park name
             var hours = data.eq(1).text(); // Get second child of .clickRow (index from 0), hours
             var emh = data.eq(2).text(); // Extra Magic Hours, third child of .clickRow
             
-            console.log(name);
-            console.log(hours);
-            console.log("\nExtra Magic Hours\n" + emh);
+            // Eliminate leading and trailing spaces
+            name = name.trim();
+            hours = hours.trim();   
+            emh = emh.trim();
+            
+            // Eliminate escape characters (\n)
+            name = unescape(name); 
+            hours = unescape(hours);
+            emh = unescape(emh);
+            
+            list.push(name, hours, emh);
+            dataList.push(list);
         });
     }
+    //console.log(dataList); For testing
 });
